a9bd74a372587596767a450ea5e106913c225251
E404ARPA <dam11.2020.jesuitas@gmail.com>
2020-12-30 20:47:47 +0100

    Initial commit

---
+++ app/src/main/java/com/example/a212/MainActivity.java
@@ -0,0 +1,119 @@
+package com.example.a212;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Menu;
+import android.widget.Toast;
+
+import com.example.a212.ui.Activity2;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.core.view.GravityCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private AppBarConfiguration mAppBarConfiguration;
+    DrawerLayout drawer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        drawer = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        if (drawer != null) {
+            drawer.addDrawerListener(toggle);
+        }
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        if (navigationView != null) {
+            navigationView.setNavigationItemSelectedListener(this);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+    public void mailIt () {
+        Intent mailItIn = new Intent();
+
+        mailItIn.setAction(Intent.ACTION_SENDTO);
+        mailItIn.setData(Uri.parse("mailto:"));
+
+        if(mailItIn.resolveActivity(getPackageManager()) != null){
+            startActivity(mailItIn);
+        } else{
+            Toast.makeText(this, "Failed to send", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void mapOut () {
+        Uri gmmIntentUri = Uri.parse("geo:0,0?q=Centro");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        startActivity(mapIntent);
+    }
+
+    public void activityData () {
+        Intent aData = new Intent(this, Activity2.class);
+        startActivity(aData);
+    }
+
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_email:
+                drawer.closeDrawer(GravityCompat.START);
+                mailIt();
+                return true;
+            case R.id.nav_map:
+                drawer.closeDrawer(GravityCompat.START);
+                mapOut();
+                return true;
+            case R.id.nav_data:
+                drawer.closeDrawer(GravityCompat.START);
+                activityData();
+                return true;
+        }
+
+
+        return false;
+    }
+}
